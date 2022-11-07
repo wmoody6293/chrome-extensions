8d115c2ac6cf738e90b41bae2ebb18c17bdfb8e4
wmoody6293 <wmoody6293@gmail.com>
2022-11-06 22:20:27 -0500

    added folder for Google-Ad-Remover

---
+++ Google-Ad-Remover/scripts/content.js
@@ -0,0 +1,6 @@
+const topAds = document.getElementById('tvcap');
+const bottomAds = document.getElementById('bottomads');
+
+//remove ads from page
+topAds.parentNode.removeChild(topAds);
+bottomAds.parentNode.removeChild(bottomAds);
