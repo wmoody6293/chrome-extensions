cdb702928bbe72c16613c550cf83b1b771f1dbc4
Will Moody <88988154+wmoody6293@users.noreply.github.com>
2022-11-06 22:23:09 -0500

    Delete scripts directory

--- scripts/content.js
+++
@@ -1,6 +0,0 @@
-const topAds = document.getElementById('tvcap');
-const bottomAds = document.getElementById('bottomads');
-
-//remove ads from page
-topAds.parentNode.removeChild(topAds);
-bottomAds.parentNode.removeChild(bottomAds);
